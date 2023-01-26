445a4256d01f0fef84b3997f744f032e2c540ed6
pervez121 <iqbalpervez121@gmail.com>
2023-01-26 21:18:58 +0500

    allert assignment

---
+++ app.js
@@ -0,0 +1,21 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+
+alert("hello There! Welcome");
+
+// 2. Write a script to display following message on your web page.
+
+alert("Error! please Enter Valid Password");
+
+// 3. Write a script to display following message on your web page
+
+alert("Welcome to Js land.. Happy Coding")
+
+// 4. Write a script to display following messages in sequence:
+
+alert("Welcome to Js land.. Happy Coding")
+alert("Happy Coding")
+
+// 5. Generate the following message through browser’s developer console:
+
+alert("Hello... i can Run Js through my web browser console")
+
